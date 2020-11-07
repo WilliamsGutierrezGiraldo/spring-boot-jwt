cbc61093b9f199cf029ecf6cf77cc92d0312d90a
wagutierrez <wagutierrez@modusoftware.com>
2020-11-07 12:26:59 -0500

    first commit

---
+++ src/main/java/com/mainsoft/test/MainsoftTestApplication.java
@@ -0,0 +1,13 @@
+package com.mainsoft.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainsoftTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainsoftTestApplication.class, args);
+	}
+
+}
