63027e07e183289d3484aec6866d27cede8f7f82
wagutierrez <wagutierrez@modusoftware.com>
2020-11-07 12:55:28 -0500

    are added entities from model database

---
+++ src/main/java/com/mainsoft/test/persistence/entities/ClienteEntity.java
@@ -0,0 +1,33 @@
+package com.mainsoft.test.persistence.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "CLIENTE")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private String apellido;
+	private String dni;
+	private String telefono;
+	private String email;
+}
---
+++ src/main/java/com/mainsoft/test/persistence/entities/VentaEntity.java
@@ -0,0 +1,31 @@
+package com.mainsoft.test.persistence.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "VENTA")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VentaEntity implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Long idCliente;
+	private Date fecha;
+
+}
